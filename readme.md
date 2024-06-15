800abd88b9c03e2552a8ff6f784e4e72b6f50475
dcerv005 <dacervantes12@gmail.com>
2024-06-15 10:19:22 -0700

    first commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+This assignment is introducing some basic JavaScript. Here we are working with primitive data types and operators. 
---
+++ scriptA1.js
@@ -0,0 +1,17 @@
+// Task 1
+let studentName;
+let age;
+let grade;
+let attendaceStatus;
+
+// Task 2
+studentName= "David";
+age = 30;
+grade = 10;
+attendaceStatus=true;
+
+// Task 3
+console.log("Name: ", studentName);
+console.log("Age: ", age)
+console.log("Grade: ", grade)
+console.log("Is student present? ", attendaceStatus)
